f3d35ab66ec8200fed1131e31165fbf9ecc2d452
Vincent L <vincentliang_01@hotmail.com>
2020-03-18 00:19:59 -0700

    Added search

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 const profileController = require('../controllers/profileController');
 const postController = require('../controllers/postController')
+const searchController = require('../controllers/searchController')
 
 // PROFILE
 router.get('/profile/:profileID', profileController.getProfile);
@@ -19,5 +20,7 @@ router.get('/post', postController.getAllPosts)
 router.get('/post/:postID', postController.getRepliesToPost)
 router.post('/post/:profileID', postController.addPost);
 
+//SEARCH
+router.post('/search', searchController.searchPosts)
 
 module.exports = router;
