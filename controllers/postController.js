6266274272fdb7a54efe4cac64b3f6c1540f7434
DariaDimchuk <daria.dimchuk@gmail.com>
2020-03-19 13:03:28 -0700

    Next/prev work, fetch 5 at a time, buttons disabled when no content, BUT code is inefficient :(

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,10 @@
 let profileModel = require('../models/profileData');
 
+var skip = 0;
+var take = 5;
+
+
+
 exports.getAllPosts = (req, res, next) => {
     let posts = profileModel.allPosts()
     posts.then( ([data, metadata]) => {
@@ -8,6 +13,44 @@ exports.getAllPosts = (req, res, next) => {
             profileCSS: true
         }); // Shasha: changed from res.send(data) to this
     })
+}
+
+
+exports.latestPostsPagination = (req, res, next) => {    
+    //if previous button was clicked, attempt to decrement skip value (never below 0 !)
+    if(req.body.previous) {
+        let val = skip - take;
+        if(val >= 0){
+            skip = val;
+        }
+    } else { //if next button was clicked, increment skip
+        skip += take;
+    }
+    
+    let id = req.body.profileId;
+    let Profile = profileModel.getProfile(id);
+
+    let discussion = profileModel.getLatestPosts(skip, take);
+
+    discussion.then( ([latestPosts, metadata]) => {
+        let disablePrevBtn = skip == 0 ? true : false;
+        let disableNextBtn = latestPosts.length == 0 ? true : false;
+
+        Profile.then( ([data, metadata]) => {
+            
+            res.render('main-profile', 
+                { 
+                    profile: data[0],
+                    post: latestPosts,
+                    profileCSS: true ,
+                    disablePrev: disablePrevBtn,
+                    disableNext: disableNextBtn
+                }
+            );
+       });
+
+        
+    });
 }
 
 exports.getRepliesToPost = (req, res, next) => {
