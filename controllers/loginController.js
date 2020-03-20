114f49ca171de6d8f0b252c1474e991690c89251
Jnauq <justin.aq88@gmail.com>
2020-03-19 17:06:58 -0700

    working login/signup

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,8 @@
 let profileModel = require('../models/profileData')
 
 exports.verifyLogin = (req,res) => {
-    // let email = req.body.email;
-    // let password = req.body.password;
-    let email = "sw123@gmail.com"
-    let password = "password"
+    let email = req.body.email;
+    let password = req.body.password;
 
     let profileId = profileModel.login(email, password)
     profileId.then( ([data, metadata]) => {
@@ -16,39 +14,32 @@ exports.verifyLogin = (req,res) => {
     })
 }
 
+var newUser = []
 exports.createNewProfile = (req,res) => {
-    // let fname = req.body.firstname
-    // let lname = req.body.lastname
-    // let email = req.body.email
-    // let pw = req.body.password
-
-    let fname = "toby"
-    let lname = "flenderson"
-    let email = "tf@gmail.com"
-    let pw = "password"
+    let fname = req.body.firstname
+    let lname = req.body.lastname
+    let email = req.body.email
+    let pw = req.body.password
 
     let profile = profileModel.newProfile(fname, lname, email, pw)
     profile.then( ([data, metadata]) => {
-        res.send(`${data.insertId}`)  // returns profileId of new user entry
+        newUser[0] = data.insertId
+        res.render('signup', {
+            signupCSS: true,
+        })
     })
 }
 
 exports.confirmNewProfile = (req,res) => {
-    // let url = req.body.imgurl
-    // let about = req.body.about
-    // let dob = req.body.dob
-    // let country = req.body.country
-    // let profileId = req.body.profileId
-
-    let url = "https://randomuser.me/api/portraits/med/men/31.jpg"
-    let about = "HR REP"
-    let dob = "1955-10-10"
-    let country = "Brazil"
-    let profileId = 18  // need to provide profileId of new user
+    let url = req.body.imageurl
+    let about = req.body.about
+    let dob = req.body.dob
+    let country = req.body.country
+    let profileId = newUser[0]
 
     let profile = profileModel.confirmProfile(url, about, dob, country, profileId)
     profile.then( ([data, metadata]) => {
-        res.redirect(`/profile/${profileId}`) // uses profileID of new user to update row and renders
+        res.redirect(`/profile/${profileId}`)
     })
 }
 
