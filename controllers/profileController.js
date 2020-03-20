d5e67ea3757f05559e2f90f729cd088419f3eefa
DariaDimchuk <daria.dimchuk@gmail.com>
2020-03-19 21:28:02 -0700

    bad attempt to cleanup code for recent discussions section

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,23 +1,46 @@
 let profileModel = require('../models/profileData');
 
+var skip = 0;
+var take = 5;
+
+
 exports.getProfile = (req,res,next) => {
     let id = req.params.profileID;
+
+    if(req.body.previous) {
+        let val = skip - take;
+        if(val >= 0){
+            skip = val;
+        }
+    } else if(req.body.next) { //if next button was clicked, increment skip
+        skip += take;
+    }
     
-    //first time we load profile page, so grab initial latest posts
-    let discussion = profileModel.getLatestPosts(0, 5); //skip 0, take 5
-    
+    let Profile = profileModel.getProfile(id);
+    let discussion = profileModel.getLatestPosts(skip, take);
+
     discussion.then( ([latestPosts, metadata]) => {
-        let Profile = profileModel.getProfile(id);
+        let disablePrevBtn = skip == 0 ? true : false;
+        let disableNextBtn = latestPosts.length == 0 ? true : false;
+
+        console.log(latestPosts);
+        console.log(data[0]);
+
+
         Profile.then( ([data, metadata]) => {
-            
             res.render('main-profile', 
                 { 
                     profile: data[0],
                     post: latestPosts,
                     profileCSS: true ,
-                    disablePrev: true
-                });
+                    disablePrev: disablePrevBtn,
+                    disableNext: disableNextBtn
+                }
+            );
        });
 
-    })
+        
+    });
+
+
 }
